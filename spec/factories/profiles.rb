91dad59e9b951943534368258ddf5371069c100d
Aliaksei Maridashvili <amarydashvili@yahoo.ca>
2011-09-15 13:32:56 +0300

    integrated profile with user and removed unused files
    Change-Id: Ia2fa924e70564099a409fd786eb52b3337e89711

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,9 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :profile do
+      first_name "MyString"
+      last_name "MyString"
+      avatar "MyString"
+    end
+end
