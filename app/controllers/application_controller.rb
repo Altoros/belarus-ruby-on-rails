1c833be02e25e57f99334bc7fd87ffbe58769389
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-15 15:40:26 +0300

    Added rights management helpers (part2)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+  include UsersHelper
 
   def after_sign_in_path_for(resource_or_scope)
     case resource_or_scope
