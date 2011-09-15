07245e33ef41199b7cb7305d3d73334250a0918e
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-15 13:35:52 +0300

    Added assotiations and migrations between user and comment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110914061155) do
+ActiveRecord::Schema.define(:version => 20110915102937) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20110914061155) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "custom_news", :force => true do |t|
