0e34b3b8907a1909c1ef3d0747ea5fd399818e5d
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-26 18:56:46 +0300

    Refactoring permissions for comments and user management
    Permission's helper methods were moved to a separate file.
    
    Change-Id: Ie8af6a439cc93ebd28bc5daa5200f6f06f2b2e3d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
   include SessionsHelper
+  include PermissionsHelper
 
   def after_sign_in_path_for(resource_or_scope)
     case resource_or_scope
---
+++ app/helpers/permissions_helper.rb
@@ -0,0 +1,30 @@
+module PermissionsHelper
+  def can(action, subject = nil)
+    user_signed_in? and
+    case action
+      when :add_comment then true
+      when :change_comment, :remove_comment then
+        current_user == subject.user or current_user.is_admin
+      when :admin, :change_admin  then current_user.is_admin
+      else false
+    end
+  end
+
+  def filter(action, subject = nil)
+    redirect_to_login_path unless can_result = can(action, subject)
+    can_result
+  end
+
+  private
+  def redirect_to_login_path
+    if request.xml_http_request?
+      render :js => "window.location='#{ login_path }'"
+    else
+      redirect_to login_path
+    end
+  end
+
+  def admin_filter
+    filter :admin
+  end
+end
