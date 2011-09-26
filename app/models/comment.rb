522e3fb50fab63622fd5c6e6865cc59694589cc9
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-26 10:25:09 +0300

    User support for comments
    Visitor can only view comments. User can create, edit and delete his
    comments. Admin have all permissions upon every comment. Comment belongs to
    one user and displays fullname from the appropriate profile. Comments with
    users and profiles loads using eager-loading-associations.
    
    Ticket: 24
    Change-Id: I860b684d47f1c57db2d57fd2933d877fc9a037b3

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   validates_presence_of :body
-  validates_length_of :body, minimum: 5
+  validates_length_of :body, :minimum => 5
   validates_associated :custom_news
   belongs_to :custom_news
+  belongs_to :user
 end
