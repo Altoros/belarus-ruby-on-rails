4114bb57b163509b662eb59b42e49960499215a3
Alexandr Yakubenko <SmitBLR@Gmail.com>
2011-09-27 12:20:43 +0300

    User support for comments
    Visitor can only view comments. User can create, edit and delete his
    comments. Admin have all permissions upon every comment. Comment belongs to
    one user and displays fullname from the appropriate profile. Comments with
    users and profiles loads using eager-loading-associations.
    Comments is a nested resource for custom_news
    
    Ticket: 24
    Change-Id: I860b684d47f1c57db2d57fd2933d877fc9a037b3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110923131651) do
+ActiveRecord::Schema.define(:version => 20110923171534) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(:version => 20110923131651) do
 
   create_table "comments", :force => true do |t|
     t.integer  "custom_news_id"
-    t.string   "author"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "custom_news", :force => true do |t|
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(:version => 20110923131651) do
   end
 
   create_table "profiles", :force => true do |t|
-    t.integer "user_id"
-    t.string  "first_name"
-    t.string  "last_name"
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "user_tokens", :force => true do |t|
