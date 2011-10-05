865241ed8d1827073d5430f11326eee88281080d
Viktor Melnikov <heaven.out.there@gmail.com>
2011-10-05 14:53:05 +0300

    As Admin I can create new Meetup announcement.
    Create Meetup and show it in admin area.
    
    Story: 130
    Task: 154
    Change-Id: I9650334612498b2a3c6e6770483378a835b5064d

---
+++ app/controllers/admin/meetups_controller.rb
@@ -0,0 +1,25 @@
+class Admin::MeetupsController < ApplicationController
+  load_and_authorize_resource :class => Meetup
+
+  def create
+    @meetup = Meetup.new(params[:meetup])
+
+    if @meetup.save
+      flash[:notice] = I18n.t('meetup.create_ok')
+      redirect_to admin_meetups_url
+    else
+      respond_to do |format|
+        format.html  { render :action => "new" }
+        format.json  { head :ok }
+      end
+    end
+  end
+
+  def index
+    @meetup = Meetup.where("date_and_time > ?", Time.now).order('id DESC').first
+  end
+
+  def edit
+    @meetup = Meetup.find_by_id(params[:id])
+  end
+end
