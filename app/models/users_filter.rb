d61e1327d3ba89d63aa43e2d0b77bcd16ee1665e
Denis Sheleh <den.sheleh@gmail.com>
2011-10-05 15:47:09 +0300

    Users filter
    As Admin I can filter list of users for mail sending
    
    Change-Id: Ib55420ffa5c6565b9264a268629757cdf57b2168

---
+++ app/models/users_filter.rb
@@ -0,0 +1,18 @@
+class UsersFilter
+
+  def self.filters_collection
+    res = { 0 => I18n.t('labels.all') }
+    # TODO select all meetups and include them with id to result hash
+    res
+  end
+
+  def self.emails_list(filter)
+    if (filter == 0 || !filters_collection.include?(filter))
+      emails = Profile.select('DISTINCT users.email AS email').subscribed
+    else
+      # TODO select emails of users, which take part in selected meetup
+    end
+
+    emails.collect(&:email)
+  end
+end
