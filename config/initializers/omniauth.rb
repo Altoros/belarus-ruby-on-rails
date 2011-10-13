1523baa9d3c16849e50230343059c9bf02cb6ce9
Vadim Golub <vdm.golub@gmail.com>
2011-10-13 09:22:59 +0300

    LinkedIn section added to credentials.yml.example
    Change-Id: I11027ea7ada0cddf6d96aeca148e197114062464

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,10 +4,12 @@ Devise.setup do |config|
 
   credentials = CONFIG['github']
 
-  config.omniauth :github, credentials['app_id'], credentials['secret_key'], :scope => 'user,public_repo'
+  config.omniauth :github, credentials['app_id'], credentials['secret_key'],
+                  :scope => 'user,public_repo'
 
   credentials = CONFIG['facebook']
-  config.omniauth :facebook, credentials['app_id'], credentials['secret_key'], :scope => 'email'
+  config.omniauth :facebook, credentials['app_id'], credentials['secret_key'],
+                  :scope => 'email'
 
   credentials = CONFIG['vkontakte']
   config.omniauth :vkontakte, credentials['app_id'], credentials['secret_key']
@@ -15,9 +17,12 @@ Devise.setup do |config|
   credentials = CONFIG['twitter']
   config.omniauth :twitter, credentials['app_id'], credentials['secret_key']
 
-  config.omniauth :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
+  config.omniauth :google_apps, OpenID::Store::Filesystem.new('/tmp'),
+                  :domain => 'gmail.com'
 
   credentials = CONFIG['linkedin']
-  config.omniauth :linked_in, credentials['app_id'], credentials['secret_key'], :site => 'https://api.linkedin.com
-/', :authorize_path => '/uas/oauth/authorize', :access_token_path => '/uas/oauth/accessToken'
+  config.omniauth :linked_in, credentials['app_id'], credentials['secret_key'],
+                  :site => 'https://api.linkedin.com/',
+                  :authorize_path => '/uas/oauth/authorize',
+                  :access_token_path => '/uas/oauth/accessToken'
 end
