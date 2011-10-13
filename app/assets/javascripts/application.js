9bedd9eefcf069423867d34edae105e5106ccc9a
Vadim Golub <vdm.golub@gmail.com>
2011-10-13 14:53:34 +0300

    Application layout added
    Change-Id: I58b12dececb0a4f625a3c81c8638ee5ee7649a21

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,48 @@ function comment_notify(message) {
         },2000)
     });
 }
+
+jQuery(document).ready(function(){
+    VK.init({
+        apiId: 2323414,
+        onlyWidgets: true
+    });
+    VK.Widgets.Group("vk_groups", {
+        mode: 0,
+        width: "250",
+        height: "250"
+    }, 26128021);
+
+    new TWTR.Widget({
+        id: 'twitter_feed',
+        version: 2,
+        type: 'search',
+        search: '#hacby',
+        interval: 30000,
+        title: 'Что пишут о',
+        subject: 'Belarus Ruby on Rails',
+        width: 250,
+        height: 300,
+        theme: {
+            shell: {
+                background: '#6b0000',
+                color: '#ffffff'
+            },
+            tweets: {
+                background: '#ffffff',
+                color: '#884467',
+                links: '#6b0000'
+            }
+        },
+        features: {
+            scrollbar: false,
+            loop: true,
+            live: true,
+            hashtags: true,
+            timestamp: true,
+            avatars: true,
+            toptweets: true,
+            behavior: 'default'
+        }
+    }).render().start();
+});
