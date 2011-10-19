2d2d39eb14a31d3a01581d3e52c62df43cad271d
Denis Sheleh <den.sheleh@gmail.com>
2011-10-19 21:01:05 +0300

    CSV Export
    As Admin I can export users statistics to CSV file using some filters
    like 'All' or meetup
    
    Ticket: 321
    Story: 299
    Change-Id: Id14e4f83ac4498c8bbd0a7cf5f0bc8ff8d8be8b1

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,9 +2,11 @@ class Admin::UsersController < ApplicationController
   load_and_authorize_resource :class => User
 
   def index
-    @users = User.paginate(:per_page => 10,
-                    :page => params[:page],
-                    :order => 'created_at DESC')
+    respond_to do |format|
+      format.html { @users = User.joins(:profile).merge(Profile.filter(params[:filters]))
+        .paginate(:per_page => 10, :page => params[:page], :order => 'users.created_at DESC') }
+      format.csv { export_csv(params[:filters]) }
+    end
   end
 
   def update
@@ -17,5 +19,13 @@ class Admin::UsersController < ApplicationController
     end
 
     redirect_to :action => :index
+  end
+
+  protected
+
+  def export_csv(filters)
+    filename = I18n.l(Time.now, :format => :short) + "- Users.csv"
+    content = User.to_csv(User.joins(:profile).merge(Profile.filter(filters)))
+    send_data content, :filename => filename
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ BelarusRubyOnRails::Application.configure do
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 #  # A dummy setup for development - no deliveries, but logged
 #  config.action_mailer.delivery_method = :smtp
-#  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = false
 #  config.action_mailer.raise_delivery_errors = true
 #  config.action_mailer.default :charset => 'utf-8',
 #                               :from => 'no-reply@belarusrubyonrails.org'
