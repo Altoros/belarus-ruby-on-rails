54634974667e4583b183dd37b91fa028eb5d537f
Denis Sheleh <den.sheleh@gmail.com>
2011-10-19 21:01:05 +0300

    CSV Export
    As Admin I can export users statistics to CSV file using some filters
    like 'All' or meetup
    
    Ticket: 321
    Story: 299
    Change-Id: Id14e4f83ac4498c8bbd0a7cf5f0bc8ff8d8be8b1

--- spec/controllers/admin/user_manager_controller_spec.rb
+++ spec/controllers/admin/user_manager_controller_spec.rb
@@ -15,19 +15,47 @@ describe Admin::UsersController do
   end
 
   describe "For admins" do
+    let(:admin) { Factory(:user, :is_admin => true) }
+    before(:each) do
+      sign_in admin
+    end
+
     it "should show user list" do
-      @user = Factory(:user, :is_admin => true)
-      sign_in @user
       get 'index'
       response.should be_success
     end
 
     it "should change admins" do
-      @user = Factory(:user, :is_admin => true)
-      sign_in @user
       user = Factory(:user)
       put 'update', { :id => user.id }
       User.find(user.id).is_admin?.should be_true
+    end
+
+    context 'downloaded CSV file' do
+      before(:each) do
+        @meetup = Factory(:meetup)
+        @exp = Factory(:experience)
+        @first_user = Factory(:user, :profile => Factory(:profile, :subscribed => true, :experience => @exp))
+        @second_user = Factory(:user, :profile => Factory(:profile,:subscribed => true, :experience => @exp))
+
+        Participant.create(:meetup_id => @meetup.id, :user_id => @first_user.id)
+      end
+
+      it 'should contain all users for "All" filter' do
+        post 'index', { :format => 'csv', :filters => [UsersFilter::ALL_OPTION] }
+        response.header['Content-Disposition'].should include('attachment')
+        response.header['Content-Disposition'].should include('csv')
+        response.body.should include(@first_user.email)
+        response.body.should include(@second_user.email)
+      end
+
+      it 'should contain only participants of meetup for selected filter' do
+        post 'index', { :format => 'csv', :filters => ["#{ @meetup.id}"] }
+        response.header['Content-Disposition'].should include('attachment')
+        response.header['Content-Disposition'].should include('csv')
+        response.body.should include(@first_user.email)
+        response.body.should_not include(@second_user.email)
+      end
     end
   end
 end
