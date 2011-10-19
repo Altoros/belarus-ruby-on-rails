e377d2b93cb57ff02cca4c4f6fa9fe4bc2e8bd56
Denis Sheleh <den.sheleh@gmail.com>
2011-10-19 21:01:05 +0300

    CSV Export
    As Admin I can export users statistics to CSV file using some filters
    like 'All' or meetup
    
    Ticket: 321
    Story: 299
    Change-Id: Id14e4f83ac4498c8bbd0a7cf5f0bc8ff8d8be8b1

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load CSV library
+require "csv"
+
 # Load config for social services
 SOCIAL_CONFIG = YAML.load_file("#{Rails.root}/config/social_config.yml")[Rails.env]
 
 # Initialize the rails application
 BelarusRubyOnRails::Application.initialize!
+
