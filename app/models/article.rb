5c6e554cd91c379bc07e4dfe50c09f45bd422312
Aliaksei Maridashvili <amarydashvili@yahoo.ca>
2011-10-20 10:12:34 +0300

    Added association between articles and users
    From now on an article view will contain it's author name.
    Change-Id: I152628df41ad21cd55f07039493d429348211839
    Story: 295
    Task: 317

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,12 @@ class Article < ActiveRecord::Base
   validates :title, :length => {:maximum => 255}
   validates :title, :presence => true
   validates :content, :presence => true
+  validates :user_id, :presence => true
   has_many :comments, :order => "created_at DESC", :dependent => :delete_all
 
+  belongs_to :user
+  validates_associated :user
+  attr_protected :user_id
   scope :published, where(:published => true)
 
   def normalize_friendly_id(text)
