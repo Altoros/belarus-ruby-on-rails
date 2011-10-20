c404110b3fbbc09e3c2dfafb4f88d69472205e81
Aliaksei Maridashvili <amarydashvili@yahoo.ca>
2011-10-20 10:12:34 +0300

    Added association between articles and users
    From now on an article view will contain it's author name.
    Change-Id: I152628df41ad21cd55f07039493d429348211839
    Story: 295
    Task: 317

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111011122020) do
+ActiveRecord::Schema.define(:version => 20111019160226) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20111011122020) do
     t.boolean  "published"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "ckeditor_assets", :force => true do |t|
