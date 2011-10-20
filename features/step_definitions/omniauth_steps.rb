0f53e3e84449817f1dd83dcb3b1b435e78e8d566
Aliaksei Maridashvili <amarydashvili@yahoo.ca>
2011-10-20 10:12:34 +0300

    Added association between articles and users
    From now on an article view will contain it's author name.
    Change-Id: I152628df41ad21cd55f07039493d429348211839
    Story: 295
    Task: 317

--- features/step_definitions/omniauth_steps.rb
+++ features/step_definitions/omniauth_steps.rb
@@ -1,4 +1,4 @@
 Given /^I come to article page with name "([^"]*)"$/ do |title|
-  cn = Factory(:article, {:title => title, :content => "Some interesting news"})
+  cn = Factory(:article, {:title => title, :content => "Some interesting news", :user => Factory(:user)})
   visit('/articles/'+cn.id.to_s)
 end
