25a5b1edc909a418772f8966c3740d79ba93fd80
Denis Sheleh <den.sheleh@gmail.com>
2011-10-24 15:33:34 +0300

    Avatars from Socila Networks
    As User when I log in via social network my profile should use its
    userpic
    
    Ticket: 339
    Story: 301
    Change-Id: Ib97ae2ee84f0c8e29d5bde503075dd29e10666a6

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,12 @@ class User < ActiveRecord::Base
       build_profile unless profile
       profile.first_name = omniauth['user_info']['first_name'] if profile.first_name.blank?
       profile.last_name = omniauth['user_info']['last_name'] if profile.last_name.blank?
+      if omniauth['user_info']['image'].present? && !profile.avatar.exists?
+        require "open-uri"
+        io = open(URI.parse(omniauth['user_info']['image'].sub("square","large")))
+        def io.original_filename; Digest::MD5.hexdigest(rand.to_s) + '.jpg'; end;
+        profile.avatar = io
+      end
 
       if omniauth.include?('provider') && omniauth.include?('uid')
         user_tokens.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
