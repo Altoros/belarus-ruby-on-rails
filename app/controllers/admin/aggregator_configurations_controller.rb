60d77b827b8dc0fd7e5b48512053be28e0bcc431
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2011-10-28 12:56:40 +0300

    Fix aggregated configuration
    Change-Id: I7f93a32e3b1a4d1f4d273d49ef5dfcde2747a39d

--- app/controllers/admin/aggregator_configurations_controller.rb
+++ app/controllers/admin/aggregator_configurations_controller.rb
@@ -2,7 +2,17 @@ class Admin::AggregatorConfigurationsController < ApplicationController
   load_and_authorize_resource
 
   def edit
-    @aggregator_configuration = AggregatorConfiguration.first
+    @aggregator_configuration = AggregatorConfiguration.first || AggregatorConfiguration.new
+  end
+
+  def create
+    respond_to do |format|
+      if AggregatorConfiguration.create(params[:aggregator_configuration])
+        format.html { redirect_to(admin_root_path, :notice => t('aggregator_configurations.successfully_updated')) }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
   end
 
   def update
@@ -10,13 +20,9 @@ class Admin::AggregatorConfigurationsController < ApplicationController
 
     respond_to do |format|
       if @aggregator_configuration.update_attributes(params[:aggregator_configuration])
-        format.html { redirect_to edit_admin_aggregator_configuration_path(@aggregator_configuration),
-                      :notice => t('aggregator_configurations.successfully_updated') }
-        format.json { head :ok }
+        format.html { redirect_to admin_root_path, :notice => t('aggregator_configurations.successfully_updated') }
       else
         format.html { render :action => "edit" }
-        format.json { render :json => @aggregator_configuration.errors,
-                      :status => :unprocessable_entity }
       end
     end
   end
