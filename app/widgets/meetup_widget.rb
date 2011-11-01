1febea1777064e5e2f50b7c6b0ec2728c3488cc0
Denis Sheleh <den.sheleh@gmail.com>
2011-11-01 12:59:45 +0300

    Fix bug: Truncate meetup description
    Change-Id: I99bcf224a5d149cc423adddfa0447e5d7b47335b

--- app/widgets/meetup_widget.rb
+++ app/widgets/meetup_widget.rb
@@ -4,6 +4,9 @@ class MeetupWidget < Apotomo::Widget
   responds_to_event :register
   after_initialize :setup!
 
+  DESCRIPTION_LENGTH = 300
+  DESCRIPTION_OMISSION = '<span id="meetup-omission">...</span>'
+
   def display
     if @meetup
       @share = { :title => @meetup.topic, :description => @meetup.description }
@@ -16,6 +19,16 @@ class MeetupWidget < Apotomo::Widget
   def register
     participant = @meetup.participants.create(:user_id => @current_user.id)
     render :text => "$('.attend_button').replaceWith($('<img src=\"#{ asset_path('you-participate.png') }\" />'))" if participant
+  end
+
+  def self.description_html(description, length = 300)
+    res = ''
+    truncated_description = truncate(description, :length => length, :separator => ' ', :omission => '<span id="meetup-omission">...</span>')
+    another_part = description.gsub(truncated_description[0..-38], '')
+    res << "<p>#{ truncated_description }"
+    res << "<a href='#' id='meetup-read-more'>More</a>"
+    res << "<span id='meetup-description-full' style='display: none;'>#{ another_part }</span></p>"
+    res.html_safe
   end
 
   private
